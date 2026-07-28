951a0177d0cf3f4e08666fdbad58a0130dd05751
YuHengSong <yuheng.song@my.jcu.edu.au>
2026-07-28 11:35:15 +0800

    Update urls.py

--- MoviesRecommend/movie/urls.py
+++ MoviesRecommend/movie/urls.py
@@ -4,28 +4,28 @@ from . import views
 
 app_name = 'movie'
 
-# 子路由
+# Sub-Routes
 urlpatterns = [
-    # 默认首页
+    # Default Home Page
     path('', views.IndexView.as_view(), name='index'),
-    # 热门电影
+    # Popular Movies
     path('hot', views.PopularMovieView.as_view(), name='hot'),
-    # 登录
+    # Login
     path('login', views.LoginView.as_view(), name='login'),
-    # 退出
+    # Log Out
     path('logout', views.UserLogout, name='logout'),
-    # 注册
+    # Registration
     path('register', views.RegisterView.as_view(), name='register'),
-    # 分类查看
+    # Browse by Category
     path('tag', views.TagView.as_view(), name='tag'),
-    # 搜索功能
+    # Search Function
     path('search', views.SearchView.as_view(), name='search'),
-    # 电影详情页面
+    # Movie Details Page
     path('detail/<int:pk>', views.MovieDetailView.as_view(), name='detail'),
-    # 评分历史页面
+    # Rating History Page
     path('history/<int:pk>', views.RatingHistoryView.as_view(), name='history'),
-    # 删除记录
+    # Delete Record
     path('del_rec/<int:pk>', views.delete_recode, name='delete_record'),
-    # 推荐页面
+    # Recommendation Page
     path('recommend', views.RecommendMovieView.as_view(), name='recommend')
 ]
