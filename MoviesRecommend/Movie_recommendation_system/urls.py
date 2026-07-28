f19ee1f17da2b85e4007bf3b01d38396c3af99df
YuHengSong <yuheng.song@my.jcu.edu.au>
2026-07-28 10:39:46 +0800

    Update urls.py

--- MoviesRecommend/Movie_recommendation_system/urls.py
+++ MoviesRecommend/Movie_recommendation_system/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from .views import index, star
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # 后台管理系统
-    path('', index),  # 首页
-    path('movie/', include('movie.urls')),  # 电影推荐系统子路由
+    path('admin/', admin.site.urls),  # Admin Management System
+    path('', index),  # Home Page
+    path('movie/', include('movie.urls')),  # Movie Recommendation System Sub-Routes
 ]
