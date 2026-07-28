5504d7d730e5d57de23e8670c106f3f3ea4fde1b
YuHengSong <yuheng.song@my.jcu.edu.au>
2026-07-28 10:57:44 +0800

    Update admin.py

--- MoviesRecommend/movie/admin.py
+++ MoviesRecommend/movie/admin.py
@@ -2,90 +2,90 @@ from django.contrib import admin
 
 from movie.models import User, Movie, Genre, Movie_hot, Movie_rating, Movie_similarity
 
-admin.site.site_title = "混合算法电影推荐系统后台管理系统"
-admin.site.site_header = "电影推荐系统-后台管理系统"
-admin.site.index_title = "混合算法电影推荐系统"
+admin.site.site_title = "Hybrid Movie Recommendation System Admin Panel"
+admin.site.site_header = "Movie Recommendation System - Admin Panel"
+admin.site.index_title = "Hybrid Algorithm Movie Recommendation System"
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    # 设置列表中显示的字段
+    # Set the Fields Displayed in the List
     list_display = ['id', 'name', 'password', 'email']
-    # 搜索
+    # Search
     search_fields = ['name', 'email']
-    # 过滤
+    # Filter
     # list_filter = ['name']
-    # 设置每页现实的数据量
+    # Set the Number of Items Displayed per Page
     list_per_page = 12
-    # 设置排序
+    # Configure Sorting
     ordering = ['id']
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    # 设置列表中显示的字段
+    # Configure Fields Displayed in the List
     list_display = ['id', 'name']
-    # 搜索
+    # Search
     search_fields = ['name']
-    # 过滤
+    # Filter
     # list_filter = ['name']
-    # 设置每页现实的数据量
+    # Set the Number of Items Displayed per Page
     list_per_page = 12
-    # 设置排序
+    # Configure Sorting
     ordering = ['id']
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    # 设置列表中显示的字段
+    # Configure Fields Displayed in the List
     list_display = ['id', 'name', 'imdb_id', 'time', 'release_time', 'intro', 'director', 'writers', 'actors', ]
-    # 搜索
+    # Search
     search_fields = ['name', 'intro', 'writers', 'actors']
-    # # 过滤
+    # # Filter
     # list_filter = ['name', 'writers']
-    # 设置每页现实的数据量
+    # Set the Number of Items Displayed per Page
     list_per_page = 6
-    # 设置排序
+    # Configure Sorting
     ordering = ['id']
 
 
 @admin.register(Movie_hot)
 class Movie_hotAdmin(admin.ModelAdmin):
-    # 设置列表中显示的字段
+    # Configure Fields Displayed in the List
     list_display = ['id', 'movie', 'rating_number']
-    # 搜索
+    # Search
     search_fields = ['movie__name']
-    # # 过滤
+    # # Filter
     # list_filter = ['name', 'writers']
-    # 设置每页现实的数据量
+    # Set the Number of Items Displayed per Page
     list_per_page = 6
-    # 设置排序
+    # Configure Sorting
     ordering = ['-rating_number']
 
 
 @admin.register(Movie_rating)
 class Movie_ratingAdmin(admin.ModelAdmin):
-    # 设置列表中显示的字段
+    # Configure Fields Displayed in the List
     list_display = ['id', 'user', 'movie', 'score', 'comment']
-    # 搜索
+    # Search
     search_fields = ['user__name', 'movie__name']
-    # # 过滤
+    # # Filter
     # list_filter = ['name', 'writers']
-    # 设置每页现实的数据量
+    # Set the Number of Items Displayed per Page
     list_per_page = 6
-    # 设置排序
+    # Configure Sorting
     ordering = ['-score']
 
 
 @admin.register(Movie_similarity)
 class Movie_similarityAdmin(admin.ModelAdmin):
-    # 设置列表中显示的字段
+    # Configure Fields Displayed in the List
     list_display = ['id', 'movie_source', 'movie_target', 'similarity']
-    # 搜索
+    # Search
     search_fields = ['movie_source__name', 'movie_source__name']
-    # # 过滤
+    # # Search
     # list_filter = ['name', 'writers']
-    # 设置每页现实的数据量
+    # Set the Number of Items Displayed per Page
     list_per_page = 6
-    # 设置排序
+    # Configure Sorting
     ordering = ['-similarity']
